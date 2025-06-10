8d1459860989e5f257206dcb0abd9a54293e0f17
Vinicius Sakai <vinicius.sakai@tuta.io>
2025-06-10 15:35:48 -0300

    feat: update LessonController and LessonService to use proper types, add Injectable decorator to SubjectService

--- src/modules/core/lessons/lesson.controller.ts
+++ src/modules/core/lessons/lesson.controller.ts
@@ -1,5 +1,9 @@
+import { Controller } from '@nestjs/common';
+import { LessonService } from './lesson.service';
+
+@Controller('lessons')
 export class LessonController {
-  public constructor(private readonly lessonService: any) {}
+  public constructor(private readonly lessonService: LessonService) {}
 
   public getLessonsBySubject(subjectId: string) {}
 
